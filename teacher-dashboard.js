baec0eeaca0d7d41a869256388f5fb1af331078c
rrraymund15-tech <rrraymund15@gmail.com>
2026-08-11 22:06:42 +0800

    Update teacher-dashboard.js

--- teacher-dashboard.js
+++ teacher-dashboard.js
@@ -1,3 +1,17 @@
+import {
+    getAuth,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+        window.location.href = "teacher.html";
+    }
+
+});
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
 import {
